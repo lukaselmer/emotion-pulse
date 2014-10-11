1a8163dc13156717c0cd1c6e727b8b9281849aae
Diego P. Steiner <diego.steiner@renuo.ch>
2014-10-11 13:02:25 +0200

    Service

--- mobile/src/main/java/ch/renuo/emotionpulse/ActivityTracker.java
+++ mobile/src/main/java/ch/renuo/emotionpulse/ActivityTracker.java
@@ -1,7 +1,21 @@
 package ch.renuo.emotionpulse;
 
 import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.accessibility.AccessibilityEvent;
+
 
 
 /**
@@ -25,6 +39,18 @@ public class ActivityTracker  extends AccessibilityService {
 
         eventText = eventText + event.getContentDescription();
 
+        Log.d(ACCESSIBILITY_SERVICE, eventText);
+
+    }
+
+    @Override
+    public void onServiceConnected() {
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        info.eventTypes = AccessibilityEvent.TYPES_ALL_MASK;
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
+        info.notificationTimeout = 0;
+        info.flags = AccessibilityServiceInfo.DEFAULT;
+        setServiceInfo(info);
     }
 
     @Override
