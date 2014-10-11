f7febec6e83e695155ec7d9bfece3777b176e362
Lukas Elmer <lukas.elmer@gmail.com>
2014-10-11 13:49:53 +0200

    added user pointer to emotion

--- mobile/src/main/java/ch/renuo/emotionpulse/EmotionService.java
+++ mobile/src/main/java/ch/renuo/emotionpulse/EmotionService.java
@@ -2,6 +2,7 @@ package ch.renuo.emotionpulse;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 /**
  * Created by lukas on 11/Oct/14.
@@ -12,6 +13,7 @@ public class EmotionService {
         o.put("heartBeat", emotion.getHeartBeat());
         o.put("application", emotion.getApplication());
         o.put("context", emotion.getContext());
+        o.put("userId", ParseUser.getCurrentUser().getRelation());
         o.save();
     }
 }
